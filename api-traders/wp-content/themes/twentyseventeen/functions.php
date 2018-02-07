8ac2befb4c688207ea9ea96c1c30373974392f0d
Kostryukov Alexey <Alexey.Kostryukov@digitalarrowtech.com>
2018-02-07 11:58:42 +0300

    + move definitions

--- api-traders/wp-content/themes/twentyseventeen/functions.php
+++ api-traders/wp-content/themes/twentyseventeen/functions.php
@@ -585,18 +585,5 @@ require get_parent_theme_file_path( '/inc/customizer.php' );
  */
 require get_parent_theme_file_path( '/inc/icon-functions.php' );
 
-include get_parent_theme_file_path( '/traders/api-manager.php' );
-include get_parent_theme_file_path( '/traders/vendor/autoload.php' );
-
-use \Binance;
-
-$api = new Binance\API("5EfGtW6SJflYpuiMeCR5QNCMteC2MquEOPD22Yqt89l59wL9fW1xA3GjgCvv9gqi","pf7HKOC40Cpj7c6dFX0vxq7V8tl3f7L3C9AVvbud3GPuPcqm5OtCsBjfFhi38hTh");
-
-//$api->chart(["BNBBTC"], "15m", function($api, $symbol, $chart) {
-    //error_log("{$symbol} chart update\n");
-    //error_log(json_encode($chart));
-//});
-$api->trades(["BNBBTC"], function($api, $symbol, $trades) {
-    error_log("{$symbol} trades update\n");
-    error_log(json_encode($trades));
-});
+require get_parent_theme_file_path( '/traders/api-manager.php' );
+require get_parent_theme_file_path( '/traders/wss-manager.php' );
